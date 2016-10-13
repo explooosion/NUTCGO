70bdda305390c3146ee77f7ec25531537c964907
Robby <ta7382@gmail.com>
2016-10-13 16:30:41 +0800

    use Node.js

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+var navItem = require('../db/navItem.json');
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+ 
+  res.render('index', { title: '建物資訊', key: 'building', navItem: navItem });
+});
+
+router.get('/building', function(req, res, next) {
+  res.render('index', { title: '建物資訊', key: 'building', navItem: navItem });
+});
+
+router.get('/art', function(req, res, next) {
+  res.render('index', { title: '藝術雕像', key: 'art', navItem: navItem });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('index', { title: '行政單位', key: 'admin', navItem: navItem });
+});
+
+router.get('/traffic', function(req, res, next) {
+  res.render('index', { title: '交通規劃', key: 'traffic', navItem: navItem });
+});
+
+router.get('/sport', function(req, res, next) {
+  res.render('index', { title: '運動休憩', key: 'sport', navItem: navItem });
+});
+
+router.get('/map', function(req, res, next) {
+  res.render('index', { title: '地圖定位', key: 'map', navItem: navItem });
+});
+
+router.get('/room', function(req, res, next) {
+  res.render('index', { title: '校園入口', key: 'room', navItem: navItem });
+});
+
+module.exports = router;
