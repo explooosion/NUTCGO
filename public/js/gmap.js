240aa1888cf5ebb9fbf47971ccdc7d05c658df7c
Robby <ta7382@gmail.com>
2017-03-16 17:04:55 +0800

    NB

--- public/js/gmap.js
+++ public/js/gmap.js
@@ -242,6 +242,7 @@ function PolygonList() {
             console.log(response);
             for (var i in response) {
                 $('#tbPolygonList').append('<tr><td>' + response[i].PolygonName + '</td><td>' + response[i].PolygonGroup + '</td><td><a href="javascript:PolygonKeySearch(' + response[i].id + ');"><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i></a></td><td><a hre' +
+                        'f="javascript:PolygonKeyEdit(' + response[i].id + ');"><i class="fa fa-pencil-square  fa-lg" aria-hidden="true"></i></a></td><td><a hre' +
                         'f="javascript:PolygonDelete(' + response[i].id + ');"><i class="fa fa-trash fa-lg" aria-hidden="true"></i></a></td></tr>');
 
             }
@@ -417,7 +418,11 @@ function PolygonKeySearch(value) {
     console.log('p search');
 }
 
-function PolygonDelete(value){
+function PolygonKeyEdit(value) {
+    console.log('p edit');
+}
+
+function PolygonDelete(value) {
     console.log('p delete');
 }
 
